d4e807d901d4e9cb76bf5ee255c9fcaf6b8d70a5
WaridIIUC <warid.cse.iiuc@gmail.com>
2021-05-01 16:01:36 +0600

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import NavbarTop from '../NavbarTop/NavbarTop';
+
+
+const Home = () => {
+    return (
+        <div>
+            <NavbarTop></NavbarTop>
+            <Header></Header>
+            <h1>Home</h1>
+        </div>
+    );
+};
+
+export default Home;
